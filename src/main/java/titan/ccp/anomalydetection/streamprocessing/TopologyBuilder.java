a8c3936eb6ef20364d80c51f1bf96495084b0269
Nico Biernat <stu209212@mail.uni-kiel.de>
2019-12-31 12:20:06 +0100

    add writing to cassandra and correct logging, also making sure that the tables exist beforehand

--- src/main/java/titan/ccp/anomalydetection/streamprocessing/TopologyBuilder.java
+++ src/main/java/titan/ccp/anomalydetection/streamprocessing/TopologyBuilder.java
@@ -1,13 +1,21 @@
 package titan.ccp.anomalydetection.streamprocessing;
 
 import com.datastax.driver.core.Session;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import kieker.common.record.IMonitoringRecord;
+
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import titan.ccp.anomalydetection.streamprocessing.detection.IAnomalyDetection;
 import titan.ccp.common.cassandra.CassandraWriter;
 import titan.ccp.common.cassandra.ExplicitPrimaryKeySelectionStrategy;
 import titan.ccp.common.cassandra.PredefinedTableNameMappers;
@@ -18,16 +26,18 @@ import titan.ccp.models.records.ActivePowerRecordFactory;
 import titan.ccp.models.records.AggregatedActivePowerRecord;
 import titan.ccp.models.records.AggregatedActivePowerRecordFactory;
 
+
 /**
  * Builds Kafka Stream Topology for the Anomaly-Detection microservice.
  */
-public final class TopologyBuilder {
+public class TopologyBuilder {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TopologyBuilder.class);
 
     private final String inputTopic;
     private final String outputTopic;
     private final Session cassandraSession;
+    private final String tableNameSuffix;
     private final IAnomalyDetection anomalyDetection;
 
     private final StreamsBuilder builder = new StreamsBuilder();
@@ -35,11 +45,14 @@ public final class TopologyBuilder {
     /**
      * Create a new {@link TopologyBuilder} using the given topics.
      */
-    public TopologyBuilder(final String inputTopic, final String outputTopic, final Session cassandraSession) {
+    public TopologyBuilder(final String inputTopic, final String outputTopic,
+                           final Session cassandraSession, final String tableNameSuffix,
+                           final IAnomalyDetection anomalyDetection) {
         this.inputTopic = inputTopic;
         this.outputTopic = outputTopic;
         this.cassandraSession = cassandraSession;
-        this.anomalyDetection = new AnomalyDetection();
+        this.tableNameSuffix = tableNameSuffix;
+        this.anomalyDetection = anomalyDetection;
     }
 
     /**
@@ -50,43 +63,65 @@ public final class TopologyBuilder {
                 = buildCassandraWriter(ActivePowerRecord.class);
         final CassandraWriter<IMonitoringRecord> aggregatedCassandraWriter
                 = buildCassandraWriter(AggregatedActivePowerRecord.class);
+        createTableIfNotExists(normalCassandraWriter,
+                new ActivePowerRecord("",0,0));
+        createTableIfNotExists(aggregatedCassandraWriter,
+                new AggregatedActivePowerRecord("",0,0,0,0,0,0));
         this.builder
                 .stream(this.inputTopic, Consumed.with(
                         Serdes.String(),
                         IMonitoringRecordSerde.serde(new ActivePowerRecordFactory())))
-                // TODO Logging
-//                .peek((k, record) -> LOGGER.info("Write ActivePowerRecord to Cassandra {}",
-//                        this.buildActivePowerRecordString(record)))
                 .filter(anomalyDetection.activePowerRecordAnomalyDetection()::test)
-                .peek((key, record) -> System.out.println("Anomaly detected! Writing record: "+buildActivePowerRecordString(record)+" to Cassandra"));
-                //.foreach((key, record) -> // todo: write to cassandra (in correct table)!);
+                .peek((key, record) ->
+                        LOGGER.info("Anomaly detected! Writing ActivePowerRecord {} to Cassandra\n",
+                                    buildActivePowerRecordString(record)))
+                .foreach((key, record) -> normalCassandraWriter.write(record));
 
         this.builder
                 .stream(this.outputTopic, Consumed.with(
                         Serdes.String(),
                         IMonitoringRecordSerde.serde(new AggregatedActivePowerRecordFactory())))
-                // TODO Logging
-//                .peek((k, record) -> LOGGER.info("Write AggregatedActivePowerRecord to Cassandra {}",
-//                        this.buildAggActivePowerRecordString(record)))
                 .filter(anomalyDetection.aggregatedActivePowerRecordAnomalyDetection()::test)
-                .peek((key, record) -> System.out.println(buildAggActivePowerRecordString(record)));
+                .peek((key, record) ->
+                        LOGGER.info("Anomaly detected! Writing AggregatedActivePowerRecord {} to Cassandra\n",
+                                    buildAggActivePowerRecordString(record)))
+                .foreach((key, record) -> aggregatedCassandraWriter.write(record));
 
         return this.builder.build();
     }
 
     private CassandraWriter<IMonitoringRecord> buildCassandraWriter(
             final Class<? extends IMonitoringRecord> recordClass) {
-            final ExplicitPrimaryKeySelectionStrategy primaryKeySelectionStrategy =
+        final ExplicitPrimaryKeySelectionStrategy primaryKeySelectionStrategy =
                 new ExplicitPrimaryKeySelectionStrategy();
-        primaryKeySelectionStrategy.registerPartitionKeys(recordClass.getSimpleName(), "identifier");
-        primaryKeySelectionStrategy.registerClusteringColumns(recordClass.getSimpleName(), "timestamp");
+        primaryKeySelectionStrategy.registerPartitionKeys(
+                recordClass.getSimpleName() + tableNameSuffix, "identifier");
+        primaryKeySelectionStrategy.registerClusteringColumns(
+                recordClass.getSimpleName() + tableNameSuffix, "timestamp");
 
-        final CassandraWriter<IMonitoringRecord> cassandraWriter =
-                CassandraWriter.builder(this.cassandraSession, new KiekerDataAdapter())
-                        .tableNameMapper(PredefinedTableNameMappers.SIMPLE_CLASS_NAME)
-                        .primaryKeySelectionStrategy(primaryKeySelectionStrategy).build();
+        return CassandraWriter.builder(this.cassandraSession, new KiekerDataAdapter())
+                .tableNameMapper(r -> PredefinedTableNameMappers.SIMPLE_CLASS_NAME.apply(r) + tableNameSuffix)
+                .primaryKeySelectionStrategy(primaryKeySelectionStrategy)
+                .build();
+    }
 
-        return cassandraWriter;
+    /*
+     * If the tables don't exist, the API will throw an error when trying to access them.
+     * This is a really inappropriate way to handle this but the CassandraWriter does not create tables when started
+     * but only when written to.
+     * And the createTableIfNotExists method is private but I can't change the titan-ccp-commons library.
+     */
+    private void createTableIfNotExists(final CassandraWriter<IMonitoringRecord> cassandraWriter,
+                                        final IMonitoringRecord record) {
+        try {
+            final Method createTableIfNotExists = cassandraWriter.getClass()
+                    .getDeclaredMethod("createTableIfNotExists", String.class, Object.class);
+            createTableIfNotExists.setAccessible(true);
+            createTableIfNotExists.invoke(cassandraWriter,
+                    record.getClass().getSimpleName() + tableNameSuffix, record);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            LOGGER.warn(e.getStackTrace().toString());
+        }
     }
 
     private String buildActivePowerRecordString(final ActivePowerRecord record) {

ef101fb290dd15caae6e8314496e0f8a99c7ac19
Nico Biernat <stu209212@mail.uni-kiel.de>
2019-12-31 12:17:53 +0100

    add anomaly detection and tablename suffix

--- src/main/java/titan/ccp/anomalydetection/streamprocessing/KafkaStreamsBuilder.java
+++ src/main/java/titan/ccp/anomalydetection/streamprocessing/KafkaStreamsBuilder.java
@@ -1,24 +1,27 @@
 package titan.ccp.anomalydetection.streamprocessing;
 
-
 import com.datastax.driver.core.Session;
+
 import java.util.Objects;
 import java.util.Properties;
+
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsConfig;
+
+import titan.ccp.anomalydetection.streamprocessing.detection.IAnomalyDetection;
 import titan.ccp.common.kafka.streams.PropertiesBuilder;
 
 /**
  * Builder for the Kafka Streams configuration.
  */
-public final class KafkaStreamsBuilder {
+public class KafkaStreamsBuilder {
 
     private static final String APPLICATION_NAME = "titan-ccp-anomalydetection";
     private static final String APPLICATION_VERSION = "0.0.1";
 
-    // private static final Logger LOGGER = LoggerFactory.getLogger(KafkaStreamsBuilder.class);
-
+    private IAnomalyDetection anomalyDetection; // NOPMD
     private Session cassandraSession; // NOPMD
+    private String tableNameSuffix; // NOPMD
     private String bootstrapServers; // NOPMD
     private String inputTopic; // NOPMD
     private String outputTopic; // NOPMD
@@ -26,8 +29,18 @@ public final class KafkaStreamsBuilder {
     private int commitIntervalMs = -1; // NOPMD
     private int cacheMaxBytesBuff = -1; // NOPMD
 
+    public KafkaStreamsBuilder anomalyDetection(final IAnomalyDetection anomalyDetection) {
+        this.anomalyDetection = anomalyDetection;
+        return this;
+    }
+
     public KafkaStreamsBuilder cassandraSession(final Session cassandraSession) {
         this.cassandraSession = cassandraSession;
+        return this;
+    }
+
+    public KafkaStreamsBuilder tableNameSuffix(final String tableNameSuffix) {
+        this.tableNameSuffix = tableNameSuffix;
         return this;
     }
 
@@ -94,15 +107,16 @@ public final class KafkaStreamsBuilder {
         final TopologyBuilder topologyBuilder = new TopologyBuilder(
                 this.inputTopic,
                 this.outputTopic,
-                this.cassandraSession);
+                this.cassandraSession,
+                this.tableNameSuffix,
+                this.anomalyDetection);
         final Properties properties = PropertiesBuilder
                 .bootstrapServers(this.bootstrapServers)
-                .applicationId(APPLICATION_NAME + '-' + APPLICATION_VERSION) // TODO as parameter
+                .applicationId(APPLICATION_NAME + '-' + APPLICATION_VERSION)
                 .set(StreamsConfig.NUM_STREAM_THREADS_CONFIG, this.numThreads, p -> p > 0)
                 .set(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, this.commitIntervalMs, p -> p >= 0)
                 .set(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, this.cacheMaxBytesBuff, p -> p >= 0)
                 .build();
         return new KafkaStreams(topologyBuilder.build(), properties);
     }
-
 }
